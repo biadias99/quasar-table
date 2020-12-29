3f78323084a4803b9dea854d303f4d8c4a0f15f7
Bianca Dias Barbosa <bianca.barbosa@linx.com.br>
2020-12-29 19:39:28 -0300

    #1 Tabela inicial

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="glossy">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+          icon="menu"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <HelloWorld />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "LayoutDefault",
+
+  components: {
+    HelloWorld,
+  },
+
+  data() {
+    return {
+      leftDrawerOpen: false,
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,542 @@
+<template>
+  <div class="q-pa-md">
+    <q-table
+      style="height: 500px"
+      title="Produto de Venda"
+      :data="data"
+      :columns="columns"
+      row-key="index"
+      virtual-scroll
+      :pagination.sync="pagination"
+      :rows-per-page-options="[0]"
+      selection="multiple"
+      :selected.sync="selected"
+      class="my-sticky-header-column-table"
+    >
+    </q-table>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      selected: [],
+      seed: [
+        {
+          name: "Frozen Yogurt",
+          calories: 159,
+          group: "Ice cream",
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          sodium: 87,
+          calcium: "14%",
+          iron: "1%",
+        },
+        {
+          name: "Ice cream sandwich",
+          calories: 237,
+          group: "Ice cream",
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          sodium: 129,
+          calcium: "8%",
+          iron: "1%",
+        },
+        {
+          name: "Eclair",
+          calories: 262,
+          group: "French",
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          sodium: 337,
+          calcium: "6%",
+          iron: "7%",
+        },
+        {
+          name: "Cupcake",
+          calories: 305,
+          group: "Cake",
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          sodium: 413,
+          calcium: "3%",
+          iron: "8%",
+        },
+        {
+          name: "Gingerbread",
+          calories: 356,
+          group: "Christmas",
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          sodium: 327,
+          calcium: "7%",
+          iron: "16%",
+        },
+        {
+          name: "Jelly bean",
+          calories: 375,
+          group: "French",
+
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+          sodium: 50,
+          calcium: "0%",
+          iron: "0%",
+        },
+        {
+          name: "Lollipop",
+          calories: 392,
+          group: "French",
+
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+          sodium: 38,
+          calcium: "0%",
+          iron: "2%",
+        },
+        {
+          name: "Honeycomb",
+          calories: 408,
+          group: "French",
+
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+          sodium: 562,
+          calcium: "0%",
+          iron: "45%",
+        },
+        {
+          name: "Donut",
+          calories: 452,
+          group: "Cake",
+
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          sodium: 326,
+          calcium: "2%",
+          iron: "22%",
+        },
+        {
+          name: "KitKat",
+          calories: 518,
+          group: "French",
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          sodium: 54,
+          calcium: "12%",
+          iron: "6%",
+        },
+      ],
+      pagination: {
+        rowsPerPage: 0,
+      },
+      data: [],
+      columns: [
+        {
+          name: "index",
+          label: "#",
+          field: "index",
+        },
+        {
+          name: "name",
+          required: true,
+          label: "Dessert (100g serving)",
+          align: "left",
+          field: (row) => row.name,
+          format: (val) => `${val}`,
+          sortable: true,
+        },
+        {
+          name: "group",
+          align: "center",
+          label: "Group",
+          field: "group",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        { name: "fat", label: "Fat (g)", field: "fat", sortable: true },
+        { name: "carbs", label: "Carbs (g)", field: "carbs" },
+        { name: "protein", label: "Protein (g)", field: "protein" },
+        { name: "sodium", label: "Sodium (mg)", field: "sodium" },
+        {
+          name: "calcium",
+          label: "Calcium (%)",
+          field: "calcium",
+          sortable: true,
+          sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
+        },
+        {
+          name: "iron",
+          label: "Iron (%)",
+          field: "iron",
+          sortable: true,
+          sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "calories",
+          align: "center",
+          label: "Calories",
+          field: "calories",
+          sortable: true,
+        },
+        {
+          name: "actions",
+          align: "center",
+          label: "Actions",
+          field: "calories",
+          sortable: false,
+        },
+      ],
+    };
+  },
+  mounted() {
+    this.loadElements();
+  },
+  methods: {
+    loadElements() {
+      for (let i = 0; i < 500; i++) {
+        this.data = this.data.concat(this.seed.slice(0).map((r) => ({ ...r })));
+      }
+      this.data.forEach((row, index) => {
+        row.index = index;
+      });
+    },
+  },
+};
+</script>
+
+<style lang="sass">
+.my-sticky-header-column-table
+  /* height or max-height is important */
+  height: 310px
+
+  /* specifying max-width so the example can
+    highlight the sticky column on any browser window */
+  max-width: 100%
+
+  td:last-child
+    background-color: #c1f4cd !important
+
+  tr th
+    position: sticky
+    z-index: 2
+    background: #fff
+
+  thead tr:last-child th
+    top: 48px
+    z-index: 3
+
+  thead tr:first-child th
+    top: 0
+    z-index: 1
+
+  tr:last-child th:last-child
+    z-index: 3
+
+  td:last-child
+    z-index: 1
+
+  td:last-child, th:last-child
+    position: sticky
+    right: 0
+</style>
